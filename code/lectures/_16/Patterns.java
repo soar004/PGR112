845c090fcedd1b31aa05923b0aea4c1fc833992d
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2023-03-08 09:43:05 +0100

    Add Patterns class to lecture 16

---
+++ code/lectures/_16/Patterns.java
@@ -0,0 +1,7 @@
+package lectures._16;
+
+public class Patterns {
+    public static void main(String[] args) {
+        System.out.println("Her skal vi teste Factory og Builder måtene å strukturere klasser på");
+    }
+}
