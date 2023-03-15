c8d332b7d9a027ca7b1fc4b02c62622bd5d43e9a
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2023-03-15 09:37:12 +0100

    Update Lecture18 Java class

--- code/lectures/_18/Lecture18.java
+++ code/lectures/_18/Lecture18.java
@@ -1,19 +1,40 @@
 package lectures._18;
 
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Lecture18 {
     public static String date = "2023-03-15";
 
     public static void main(String[] args)
     {
         //# 1. Registrer driveren
+        try {
+            DriverManager.registerDriver(new Driver());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         //# 2. Hent tilkobling
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Lecture18?allowPublicKeyRetrieval=true&useSSL=false",
+                    "lecture18",
+                    "password18"
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         //# 3. Gjør klar uttrykk som skal kjøres
 
         //# 4. Kjør uttrykket
 
         //# 5. Rydd opp etter oss
-
     }
 }
