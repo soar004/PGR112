cb6e7985d3917a4fc09f66aec42f0ed3abc26f25
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2023-03-15 10:02:08 +0100

    Update Lecture 18 material

--- code/lectures/_18/Lecture18.java
+++ code/lectures/_18/Lecture18.java
@@ -5,6 +5,7 @@ import com.mysql.cj.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Lecture18 {
     public static String date = "2023-03-15";
@@ -32,10 +33,27 @@ public class Lecture18 {
         }
 
         //# 2.1. Sjekke om tilkoblingen fungerer
-
         System.out.println("Tilkoblingen fungerer!");
 
         //# 3. Gjør klar uttrykk som skal kjøres
+        try {
+            Statement statement = connection.createStatement();
+
+            String query = "CREATE TABLE IF NOT EXISTS users"
+            + "("
+                    + "id INT AUTO_INCREMENT, "
+                    + "username VARCHAR(64), "
+                    + "password VARCHAR(256), "
+                    + "PRIMARY KEY(id)"
+            +")";
+
+            statement.execute(query);
+
+            System.out.println(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         //# 4. Kjør uttrykket
 
