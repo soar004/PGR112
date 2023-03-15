78a92935dada5fad04748e670cf49d69f0b362b2
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2023-03-15 09:36:47 +0100

    Update SQL-file example from Lecture 18

--- code/lectures/_18/sql/create_database_and_user.sql
+++ code/lectures/_18/sql/create_database_and_user.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE PGR112Lecture18;
+CREATE DATABASE Lecture18;
 
 CREATE USER 'lecture18' IDENTIFIED BY 'password18';
 
-GRANT ALL on PGR112Lecture18.* TO 'lecture18';
+GRANT ALL on Lecture18.* TO 'lecture18';
