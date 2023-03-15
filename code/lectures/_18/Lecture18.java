3b49e3a9c28f0f1fd7798dcf42e2f7a95c9e6858
Marcus Alexander Dahl <MarcusAlexander.Dahl@kristiania.no>
2023-03-15 09:17:16 +0100

    Update Lecture 18 material

--- code/lectures/_18/Lecture18.java
+++ code/lectures/_18/Lecture18.java
@@ -3,7 +3,17 @@ package lectures._18;
 public class Lecture18 {
     public static String date = "2023-03-15";
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //# 1. Registrer driveren
+
+        //# 2. Hent tilkobling
+
+        //# 3. Gjør klar uttrykk som skal kjøres
+
+        //# 4. Kjør uttrykket
+
+        //# 5. Rydd opp etter oss
 
     }
 }
